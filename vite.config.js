2aaf74b98a15e2381a24a9714e0f89052602a044
xeonem-dotcom <flameofficial007@gmail.com>
2025-10-31 22:42:41 +0530

    Fixed Vite base path

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/for_alpika)/',   // 👈 THIS LINE IS CRUCIAL
+  base: '/for_alpika/',  
 })
 
