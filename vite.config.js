17fa92dfca9452c4a4f5a1ab8df28963bdbbf562
xeonem-dotcom <flameofficial007@gmail.com>
2025-10-31 21:55:46 +0530

    Initial clean deploy

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/for_alpika/',
   plugins: [react()],
-  base: '/for_alpika/'
 })
